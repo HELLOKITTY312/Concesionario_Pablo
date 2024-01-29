29d52ea7ccd42764c52664034f835f9dcc094e8a
HELLOKITTY <pablosccp.4@gmail.com@gmail.com>
2024-01-29 12:37:22 +0100

    Clase Vehículo Miembros privados, propiedades y constructores

--- ConcesionarioV1_Pablo/vehiculo.cs
+++ ConcesionarioV1_Pablo/vehiculo.cs
@@ -12,12 +12,27 @@ namespace ConcesionarioV1_Pablo
 
 
         //MIEMBROS PRIVADOS
-
-
-
+        private string _marca;
+        private string _modelo;
         //CONSTRUCTORES
-
-
+        public vehiculo(string marca, string modelo)
+        {
+            Marca= marca;
+            Modelo= modelo;
+        }
+        //Propiedades
+        public string Marca
+        {
+            get { return _marca; }
+            //TODO: Proporcionar seguridad y proteccion al miembro - _marca
+            set { _marca = value; }
+        }
+        public string Modelo
+        {
+            get { return _modelo; }
+            //TODO: Proporcionar seguridad y proteccion al miembro - _modelo
+            set { _modelo = value; }
+        }
         //MÉTODOS PÚBLICOS
 
 
