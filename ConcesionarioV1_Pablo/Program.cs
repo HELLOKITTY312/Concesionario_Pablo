758cb42f759047fdeecd579b85b1ca0a561516e9
HELLOKITTY <pablosccp.4@gmail.com@gmail.com>
2024-01-29 12:04:17 +0100

    Agregar archivos de proyecto.

---
+++ ConcesionarioV1_Pablo/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConcesionarioV1_Pablo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Controlador principal de la aplicación
+
+
+            //Menú de acciones
+
+
+
+            //Proceso de las acciones
+        }
+    }
+}
